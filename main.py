6256426bbac6f78264b8e1fbf42da6cd0e2ae733
Jimmy Nguyen <dtstego@gmail.com>
2025-04-06 01:52:15 -0700

    Added simple size policy changes for QoL.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QListWidget, QStackedWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QListWidget, QStackedWidget, QSizePolicy
 from core import CorePage
 
 
 class MainWindow(QMainWindow):
     """Container for different Widgets/Pages with a menu-style list for switching between pages."""
+
     def __init__(self):
         super().__init__()
 
@@ -14,12 +15,14 @@ class MainWindow(QMainWindow):
         # Widget (List) that displays clickable tabs that allows for switching pages.
         self.menu = QListWidget()
         self.menu.addItems(["Rename", "Formats", "Settings"])
+        self.menu.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
         central_widget.addWidget(self.menu)
 
         # QStackedWidget stores different pages/widgets that can be switched to/from.
         # See: https://doc.qt.io/qt-6/qstackedwidget.html.
         self.pages = QStackedWidget()
         self.pages.addWidget(CorePage())
+        self.pages.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         central_widget.addWidget(self.pages)
 
         # Connects signal for when the user clicks on a menu tab to setCurrentIndex()'s slot to change pages.
@@ -30,6 +33,10 @@ class MainWindow(QMainWindow):
         central_widget_container = QWidget()
         central_widget_container.setLayout(central_widget)
         self.setCentralWidget(central_widget_container)
+
+        # Additional window setup.
+        self.setWindowTitle("Simpler FileBot v0.1")
+        self.setMinimumSize(600, 200)
 
 
 if __name__ == "__main__":
