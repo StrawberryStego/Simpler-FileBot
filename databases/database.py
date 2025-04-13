2aad578545fb06691c4d2329d0b83da12656ab87
Jimmy Nguyen <dtstego@gmail.com>
2025-04-12 19:16:07 -0700

    Created abstract database.py.

---
+++ databases/database.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+from typing import List
+
+from backend.media_record import MediaRecord
+
+
+class Database:
+    """Abstract class to interact with a database and return data."""
+
+    def __init__(self, media_records: List["MediaRecord"], is_tv_series: bool = False):
+        self.media_records = media_records
+        self.is_tv_series = is_tv_series
+
+    @abstractmethod
+    def retrieve_media_titles_from_db(self) -> list:
+        pass
+
+    @abstractmethod
+    def retrieve_media_years_from_db(self) -> list:
+        pass
