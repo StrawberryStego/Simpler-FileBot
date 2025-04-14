e626d48a4466688e64e0a52b4dce6be0d2a4ddc2
Jimmy Nguyen <dtstego@gmail.com>
2025-04-13 18:15:54 -0700

    Refactored some variable names for clarity.

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        central_widget = QHBoxLayout()
+        central_layout = QHBoxLayout()
 
         # Widget (List) that displays clickable tabs that allows for switching pages.
         self.menu = QListWidget()
         self.menu.addItems(["Rename", "Formats", "Settings"])
         self.menu.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
-        central_widget.addWidget(self.menu)
+        central_layout.addWidget(self.menu)
 
         # QStackedWidget stores different pages/widgets that can be switched to/from.
         # See: https://doc.qt.io/qt-6/qstackedwidget.html.
@@ -29,15 +29,15 @@ class MainWindow(QMainWindow):
         self.pages.addWidget(FormatsPage())
         self.pages.addWidget(PreferencesPage())
         self.pages.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        central_widget.addWidget(self.pages)
+        central_layout.addWidget(self.pages)
 
         # Connects signal for when the user clicks on a menu tab to setCurrentIndex()'s slot to change pages.
         self.menu.currentRowChanged.connect(self.pages.setCurrentIndex)
         self.menu.setCurrentRow(0)
 
-        # Package the central_widget into a QWidget container as QMainWindow works with Widgets, not layouts.
+        # Package the central_layout into a QWidget container as QMainWindow works with Widgets, not layouts.
         central_widget_container = QWidget()
-        central_widget_container.setLayout(central_widget)
+        central_widget_container.setLayout(central_layout)
         self.setCentralWidget(central_widget_container)
 
         # Additional window setup.
