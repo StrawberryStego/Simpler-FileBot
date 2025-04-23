43646f8c68d128e309db339a707149b7630a19e5
Jimmy Nguyen <dtstego@gmail.com>
2025-04-23 04:51:24 -0700

    Added an error popup if rename or undo fails.

---
+++ backend/error_popup_widget.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
+
+
+class ErrorPopupWidget(QDialog):
+    """QDialog Widget that displays an error message for the user."""
+
+    def __init__(self, error_msg: str, parent=None):
+        super().__init__(parent)
+
+        error_popup_layout = QVBoxLayout(self)
+
+        error_msg_label = QLabel(error_msg)
+
+        confirmation_button = QPushButton("Confirm")
+        confirmation_button.clicked.connect(self.close)
+
+        error_popup_layout.addWidget(error_msg_label)
+        error_popup_layout.addWidget(confirmation_button)
