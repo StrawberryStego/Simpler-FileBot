34691b090d76ffda40e93e6f633e70ee2c3173c7
Jimmy Nguyen <dtstego@gmail.com>
2025-04-24 03:15:52 -0700

    Adds foundational UI elements for settings.py.

--- pages/settings.py
+++ pages/settings.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QComboBox, QPushButton
 
 
 class SettingsPage(QWidget):
@@ -7,8 +7,20 @@ class SettingsPage(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        preferences_page_layout = QVBoxLayout(self)
+        settings_page_layout = QVBoxLayout(self)
 
-        title = QLabel("Welcome to the Settings Page!")
+        title = QLabel("Settings")
 
-        preferences_page_layout.addWidget(title)
+        # Theme Selection UI Components.
+        theme_label = QLabel("Select Theme:")
+        self.theme_options = QComboBox()
+        self.theme_options.addItems(["Light", "Dark"])
+
+        # Reset Settings UI Components.
+        reset_button = QPushButton("Reset All Settings to Default")
+
+        settings_page_layout.addWidget(title)
+        settings_page_layout.addWidget(theme_label)
+        settings_page_layout.addWidget(self.theme_options)
+        settings_page_layout.addStretch()
+        settings_page_layout.addWidget(reset_button)
