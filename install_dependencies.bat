d11f72338b5f776c7e6b03dc260f1ba25112e2e2
Jimmy Nguyen <dtstego@gmail.com>
2025-06-14 09:35:06 -0700

    Added a check to see if Python exists for install_dependencies.bat.

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,6 +1,16 @@
 @echo off
 cd /d "%~dp0"
 
+rem -- Check if Python is available.
+python --version > nul 2>&1
+if errorlevel 1 (
+	echo.
+	echo [Error] Python was not found on this system.
+	echo Please install Python 3.9+ and make sure it's in your PATH/environment variables.
+	pause
+	exit /b 1
+)
+
 rem -- Create a venv if missing.
 if not exist venv (
 	python -m venv venv
