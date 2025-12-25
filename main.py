4ad0675850934657bb82a491124ebf21e358101e
Jimmy Nguyen <dtstego@gmail.com>
2025-12-24 17:29:20 -0800

    Fix get_unique_titles() to ignore case sensitivity.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class MainWindow(QMainWindow):
         self.menu.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum)
 
         # Additional window setup.
-        self.setWindowTitle("Simpler FileBot v1.5.2")
+        self.setWindowTitle("Simpler FileBot v1.5.3")
         self.setWindowIcon(QIcon(QPixmap("resources/Alternative App Logo.png")))
 
         # Set the main application to start at a percentage of the screen's size.
