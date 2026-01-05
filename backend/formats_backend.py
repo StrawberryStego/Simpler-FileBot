b1d98a03d8409b0f6664f63287cb6e9de4514f0d
Peter Kajan <sknitro@gmail.com>
2026-01-05 22:31:04 +0100

    pylint line limit

--- backend/formats_backend.py
+++ backend/formats_backend.py
@@ -4,8 +4,8 @@ formats_json_config = JSONConfig(
     "formats.json",
     {
         "movie_format": "{movie_name} ({year})",
-        "series_format": "{series_name} - S{season_number}E{episode_number} - {episode_title}"
-    }
+        "series_format": "{series_name} - S{season_number}E{episode_number} - {episode_title}",
+    },
 )
 
 
@@ -18,7 +18,10 @@ def save_new_movies_format_to_formats_file(new_movie_format: str):
 
 
 def retrieve_series_format_from_formats_file():
-    return formats_json_config.get("series_format", "{series_name} - S{season_number}E{episode_number} - {episode_title}")
+    return formats_json_config.get(
+        "series_format",
+        "{series_name} - S{season_number}E{episode_number} - {episode_title}",
+    )
 
 
 def save_new_series_format_to_formats_file(new_series_format: str):
